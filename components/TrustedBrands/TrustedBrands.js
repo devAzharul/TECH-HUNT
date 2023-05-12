f24f773524db59d37ab37e53aa1c623a02e3f032
Azharul Islam <lonewolfazharul@gmail.com>
2023-05-12 07:09:35 +0600

    created brand component

---
+++ components/TrustedBrands/TrustedBrands.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import Image from 'next/image';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import logo from '../../Assests/Images/Digit-Logo.png'
+import logo1 from '../../Assests/Images/logo-2.png'
+import logo2 from '../../Assests/Images/logo-3.png'
+import logo3 from '../../Assests/Images/logo-6.png'
+import logo4 from '../../Assests/Images/Logo-Digious.png'
+import logo5 from '../../Assests/Images/Logo-Youseo-10.png'
+
+const TrustedBrands = () => {
+
+    const settings = {
+        infinite: true,
+        autoplay: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1
+    };
+
+    return (
+        <div className='flex justify-evenly'>
+            <div className='w-2/5'>
+                <div className='mb-10'>
+                    <Slider {...settings}>
+                        <div>
+                            <Image alt='logo' src={logo} />
+                        </div>
+                        <div>
+                            <Image alt='logo' src={logo1} />
+                        </div>
+                        <div>
+                            <Image alt='logo' src={logo2} />
+                        </div>
+                        <div>
+                            <Image alt='logo' src={logo3} />
+                        </div>
+                        <div>
+                            <Image alt='logo' src={logo4} />
+                        </div>
+                        <div>
+                            <Image alt='logo' src={logo5} />
+                        </div>
+                    </Slider>
+                </div>
+                <div></div>
+                <div> <Slider {...settings}>
+                    <div>
+                        <Image alt='logo' src={logo} />
+                    </div>
+                    <div>
+                        <Image alt='logo' src={logo1} />
+                    </div>
+                    <div>
+                        <Image alt='logo' src={logo2} />
+                    </div>
+                    <div>
+                        <Image alt='logo' src={logo3} />
+                    </div>
+                    <div>
+                        <Image alt='logo' src={logo4} />
+                    </div>
+                    <div>
+                        <Image alt='logo' src={logo5} />
+                    </div>
+                </Slider></div>
+            </div>
+            <div>
+                <div className='mb-8'>
+                    <h2 className='text-white text-5xl font-bold'>
+                        Trusted By <span className='text-cyan-300'>1,790+</span><br /> Worlds Leading<br /><span className='text-cyan-300'> IT Companies.</span>
+                    </h2>
+                </div>
+                <div>
+                    <p className='text-white'>
+                        Consectetur adipiscing elit. Maecenas quis faucibus <br />ligula.Maecenas a purus massa. Ut elementum sem.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default TrustedBrands;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Layout from "@/components/Layout";
-import Navbar from "@/components/Navigation/Navbar";
+import TrustedBrands from "@/components/TrustedBrands/TrustedBrands";
 
 export default function Home() {
   return (
     <Layout title="Home" >
       <h1>This is our home page...content is loading...</h1>
+      <TrustedBrands></TrustedBrands>
     </Layout>
   );
 }
