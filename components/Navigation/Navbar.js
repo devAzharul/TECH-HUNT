ce902a18f902a29cbee4736e5af62b9aeca708a6
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-05-12 12:05:08 +0600

    changes something

--- components/Navigation/Navbar.js
+++ components/Navigation/Navbar.js
@@ -1,11 +1,6 @@
 import Link from "next/link";
 
 function Navbar() {
-  const [isNavOpen, setIsNavOpen] = useState(false);
-
-  const toggleNavDrawer = () => {
-    setIsNavOpen((prevState) => !prevState);
-  };
 
   return (
     <div className="navbar bg-black shadow-lg py-4">
@@ -15,40 +10,41 @@ function Navbar() {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </label>
           <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-           {menuList}
-    <div className="navbar bg-black shadow-lg py-4">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-          </label>
-          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-           {menuList}
+            {menuList}
+            <div className="navbar bg-black shadow-lg py-4">
+              <div className="navbar-start">
+                <div className="dropdown">
+                  <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                  </label>
+                  <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    {menuList}
+                  </ul>
+                </div>
+                <Link href={'/'} className={`font-bold text-xl text-white`}>TECH-HUNT</Link>
+              </div>
+              <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 text-white font-bold">
+                  {menuList}
+                </ul>
+              </div>
+              <div className="navbar-end">
+                <a className="btn">Login</a>
+              </div>
+            </div>
+            <Link href={'/'} className={`font-bold text-xl text-white`}>TECH-HUNT</Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1 text-white font-bold">
+            {menuList}
           </ul>
         </div>
-        <Link href={'/'} className={`font-bold text-xl text-white`}>TECH-HUNT</Link>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1 text-white font-bold">
-          {menuList}
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Login</a>
+        <div className="navbar-end">
+          <a className="btn">Login</a>
+        </div>
       </div>
     </div>
-        <Link href={'/'} className={`font-bold text-xl text-white`}>TECH-HUNT</Link>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1 text-white font-bold">
-          {menuList}
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Login</a>
-      </div>
-    </div>
-  );
+    )
 };
 
 export default Navbar;
