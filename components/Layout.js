b9215c91e010a236c4ddb52d043f761d46622331
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-05-12 23:15:07 +0600

    create navbar again

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import Footer from "./Footer/Footer";
+import Navbar from "./Navbar/Navbar";
 
 const Layout = ({ title, children }) => {
   return (
@@ -10,8 +11,12 @@ const Layout = ({ title, children }) => {
         <meta name="description" content="Tech Hunt" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet" />
       </Head>
       <div className="min-h-screen flex flex-col justify-between">
+        <Navbar />
         <main className="m-auto mt-0 w-full">{children}</main>
         <Footer />
       </div>
---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Navbar = () => {
+    const menuList = <>
+        <li><Link href={"/"}>Home</Link></li>
+        <li><Link href={"/service"}>Service</Link></li>
+        <li><Link href={"/about"}>About Us</Link></li>
+        <li><Link href={"/contact"}>Contact Us</Link></li>
+        <li><Link href={"/blog"}>Blog</Link></li>
+        <li><Link href={"/team"}>Team</Link></li>
+    </>
+    return (
+        <div className="navbar bg-base-100 py-4">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="text-white font-bold menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                       {menuList}
+                    </ul>
+                </div>
+                <a className="normal-case text-xl text-white font-bold">TECH-HUNT</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="text-white font-bold menu menu-horizontal px-1">
+                   {menuList}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Get started</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
