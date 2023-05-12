dab36a57cd01e18212046cb723bc75c100f6aff4
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-05-12 11:43:46 +0600

    added navbar

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import TrustedBrands from "@/components/TrustedBrands/TrustedBrands";
 export default function Home() {
   return (
     <Layout title="Home" >
-      <h1>This is our home page...content is loading...</h1>
       <TrustedBrands></TrustedBrands>
     </Layout>
   );
