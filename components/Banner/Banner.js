eeee24e29a68f6b501d51a72f521c3789511d16f
Azharul Islam <lonewolfazharul@gmail.com>
2023-05-19 06:51:33 +0600

    new section choose us

--- components/Banner/Banner.js
+++ components/Banner/Banner.js
@@ -22,7 +22,7 @@ const Banner = () => {
                         <Image src={bannerOne} alt="Banner one" />
                         <div className="absolute top-[30%] left-4 z-10">
                             <h1 className="text-white font-bold mb-8" style={{ fontSize: "7rem" }}>Creating a Better <br /> <span className="primaryText">IT Solutions.</span> </h1>
-                            <p className="text-gray-300">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                            <p className="text-gray-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis faucibus ligula. Maecenas a purus massa. Ut elementum sem mauris, ut<br />venenatis tortor congue et. Ut et dolor massa.</p>
                             <button className="primaryBtn">Contact Us</button>
                         </div>
                     </div>
@@ -32,7 +32,7 @@ const Banner = () => {
                         <Image src={bannerTwo} alt="Banner two" />
                         <div className="absolute top-[30%] left-4 z-10">
                             <h1 className="text-white font-bold mb-8" style={{ fontSize: "7rem" }}>Creating a Better <br /> <span className="primaryText">IT Solutions.</span> </h1>
-                            <p className="text-gray-300">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+                            <p className="text-gray-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis faucibus ligula. Maecenas a purus massa. Ut elementum sem mauris, ut<br />venenatis tortor congue et. Ut et dolor massa.</p>
                             <button className="primaryBtn">Contact Us</button>
                         </div>
                     </div>
