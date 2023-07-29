e656a6a475bc433aa58f9d9c085772aab1e0fc47
Azharul Islam <lonewolfazharul@gmail.com>
2023-07-29 16:37:19 +0600

    services and footer

--- pages/service.js
+++ pages/service.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Layout from "@/components/Layout";
-import style from "@/styles/Service/Service.module.css";
 import ServiceProvider from "@/components/Services/ServiceProvider";
 
 const ServiceScreen = () => {
   return (
     <Layout title="Service">
-      <ServiceProvider/>
+      <ServiceProvider />
     </Layout>
   );
 };
