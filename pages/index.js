d13219bddcd525429d824e671a1c3abfb3d77ab4
unknown <JNKarif>
2023-10-17 22:27:12 +0600

    tittle changed

--- pages/index.js
+++ pages/index.js
@@ -23,8 +23,8 @@ export default function Home() {
   return (
     <Layout>
       <Head>
-        <title>Welcome to Tech-Hunt - Your One-Stop Destination for Web Applications</title>
-        <meta name="description" content="Welcome to Tech-Hunt, your one-stop destination for web applications. Discover innovative solutions, expert services, and top-notch products tailored to meet your needs. Our mission is to provide you with the best experience and exceed your expectations." />
+        <title>Web development Company in Bangladesh</title>
+        <meta name="description" content="Tech-Hunt: Web development company in Bangladesh, your one-stop destination for web applications. Our mission is to provide you with the best experience and exceed your expectations." />
         <meta name="keywords" content="Tech-Hunt, Web Applications, Website Design, E-commerce Development, Web Hosting, Search Engine Optimization, Web Development, Front-end Development, Back-end Development, Full-stack Development, Responsive Web Design, User Experience (UX) Design, Mobile App Development, E-commerce Solutions, Content Management Systems, WordPress Development, Website Maintenance, SEO Services, UI/UX Design" />
         <meta name="robots" content="index, follow" />
         <meta property="og:title" content="Welcome to Tech-Hunt - Your One-Stop Destination for Web Applications" />
