72e772e8a09e7b37b743412a56bb843b5ed37fb6
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-10-22 12:17:23 +0600

    change in navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -18,7 +18,6 @@ const Navbar = () => {
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <FaBars size={30} className='text-white font-bold' />
-                        {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg> */}
                     </label>
                     <ul tabIndex={0} className="bg-black text-white font-bold menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-52">
                         {menuList}
